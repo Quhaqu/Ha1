0737aabeba9cb167d34621c9daed5a6b0d20de4d
Grandmasterr <phillip.vogler@campus.tu-berlin.de>
2019-04-25 00:41:20 +0200

    Adding functionality exists, deleting doesn't.

--- Phillip's Dateien/scripts.js
+++ Phillip's Dateien/scripts.js
@@ -3,11 +3,14 @@ function saveUserInput(){
 
     localStorage.taskdescription = document.getElementById("taskname").value;
     localStorage.locompletion = document.getElementById("locompletion").value;
-    localStorage.taskdescription = document.getElementById("taskname").value;
+    localStorage.deadline = document.getElementById("deadline").value;
 
-
-
+    console.log(localStorage.taskdescription);
+    console.log(localStorage.locompletion);
+    console.log(localStorage.deadline);
+    localStorage.NewTaskFlag = "1";
     document.getElementById("homepage").click();
+
 
   } else {
     document.getElementById("result").innerHTML = "Your browser is trash, looks like you gotta use pen and paper for this";
@@ -16,5 +19,34 @@ function saveUserInput(){
 
 
 function updateTableIfNecessary(){
+  if (localStorage.NewTaskFlag === "1"){      //New insertion needed?
+    //if(localStorage.actualtable){       //already a table saved?
+    if(localStorage.actualtable){
+     document.getElementById('tasktable').innerHTML = localStorage.actualtable;
+   }
+    //}
+    //else{
+    var table = document.getElementById('tasktable');
+    //}
+    var newtask = table.insertRow(1);
+    var desccell = newtask.insertCell(0);
+    var completioncell = newtask.insertCell(1);
+    var deadlinecell = newtask.insertCell(2);
+    var buttoncell = newtask.insertCell(3);
 
+    desccell.innerHTML = localStorage.taskdescription;
+    completioncell.innerHTML=localStorage.locompletion;
+    deadlinecell.innerHTML=localStorage.deadline;
+    buttoncell.innerHTML= '<button type="button" class="btn btn-primary"> X </button>' ;
+
+    localStorage.NewTaskFlag = "0";
+    localStorage.actualtable = table.outerHTML;
+    document.getElementById('tasktable').innerHTML = localStorage.actualtable;
+  }
+  else{
+    if(localStorage.actualtable){
+    document.getElementById('tasktable').innerHTML = localStorage.actualtable;
+    }
+
+  }
 }
